00601a9f06a8cf9edcc639fb53d5650924665ab3
fabio circuncisao <fabio.circuncisao@aditek.com.br>
2024-06-14 14:43:48 -0300

    auth guard module

---
+++ src/guards/auth_guards.module.ts
@@ -0,0 +1,14 @@
+// import { CanActivate, ExecutionContext, Module } from "@nestjs/common";
+// import { Observable } from "rxjs";
+
+// @Module({
+//   imports:[],
+//   providers:[],
+//   controllers:[],
+//   exports:[]
+// })
+// export class Guard implements CanActivate {
+//   // canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+      
+//   // }
+// }
